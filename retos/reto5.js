696866f5c20a50e270cf08b93f7d462f7b2dc955
Christian Eduardo <ceduardo@ccoo.cat>
2022-11-30 08:30:52 +0100

    solicuin reto 5 no hay que desordenar

--- retos/reto5.js
+++ retos/reto5.js
@@ -8,31 +8,18 @@ const url = 'https://codember.dev/mecenas.json'
 
 const challenge = (nombres) => {
   const size = nombres.length
-  console.log(size)
   const  datos = new Array(size).fill(0,0,size).map( (o,i) => o=i )
-  // Algoritmo de barajado
-  
-  for(let idx=0;idx<size-1;idx++) {
-    const num = datos[idx],
-          pos = Math.floor( Math.random() * (size-idx) ) + idx
-    datos[idx] = datos[pos]
-    datos[pos] = num
-  }
-  
-
   let pos = 0, vivo = 1
   while(pos!==vivo) {
     datos[ vivo ] = 'X'
     pos = (vivo + 1)%size
     while( datos[pos] === 'X' ) pos = (pos+1)%size
-    //const last = vivo
     vivo = (pos + 1)%size
     while( datos[vivo] === 'X' ) vivo = (vivo+1)%size
-    //if( last> vivo ) console.log(datos)
   } 
-  console.log(datos, datos[pos], nombres[datos[pos]])
+  console.log(`submit ${nombres[datos[pos]]}-${datos[pos]}`)
 }
 
-//challenge('a,b,c,d,e,f,g,h,i,j'.split(','))
+challenge('a,b,c,d,e,f,g,h,i,j'.split(',').fill('test',0,10))
 
 main()
